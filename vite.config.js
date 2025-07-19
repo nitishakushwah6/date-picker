6a4fef2c3eaaadbc27baad14739508e60cd9de01
nitishakushwah6 <nitishakushwah@6gmail.com>
2025-07-19 14:30:39 +0530

    fix: allow CodeSandbox preview host

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+   server: {
+    allowedHosts: ['.csb.app'], // allow CodeSandbox preview URLs
+  },
 })
